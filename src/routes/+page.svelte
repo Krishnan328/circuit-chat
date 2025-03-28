1a6946f851bc566948be40cd7897285033fd5eec
Suyog Tandel <127536688+RedddFoxxyy@users.noreply.github.com>
2025-03-28 22:54:56 +0530

    feat: add login page background

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div>
+    <h1>Welcome to SvelteKit</h1>
+    <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+    <img src="cat.jpg" class="fixed left-0 top-0 h-full w-full">
+    <h1> Krishnan is gay</h1>
+</div>
+
+
+
